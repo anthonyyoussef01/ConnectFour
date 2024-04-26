b6efce18d8d3af9142f469f859b00a95d3a49c63
antho <anthonyyoussef01@gmail.com>
2024-04-25 21:40:52 -0400

    Implemented GameView with the printBoard and playTurn

--- src/model/Game.java
+++ src/model/Game.java
@@ -4,11 +4,11 @@ import exceptions.ColumnIsFullException;
 import exceptions.InvalidColumnException;
 
 public class Game {
+    public final int NUM_ROWS = 6;
+    public final int NUM_COLS = 7;
     private Disc[][] board;
     private Player playerOne;
     private Player playerTwo;
-    private final int numRows = 6;
-    private final int numCols = 7;
     private boolean turn;
 
     public Game(Player playerOne, Player playerTwo) {
@@ -16,14 +16,14 @@ public class Game {
         this.playerTwo = playerTwo;
         // make sure the player who will start is a random player
         randomizePlayers();
-        this.board = new Disc[numRows][numCols];
+        this.board = new Disc[NUM_ROWS][NUM_COLS];
         this.turn = true;
     }
 
     public boolean checkIfGameIsOver() {
         // Check if the game is over
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
                 if (board[i][j] == null) {
                     return false;
                 }
@@ -50,7 +50,7 @@ public class Game {
 
     private boolean checkHorizontal(int row, Player turnPlayer) {
         int count = 0;
-        for (int i = 0; i < this.numCols; i++) {
+        for (int i = 0; i < this.NUM_COLS; i++) {
             if (this.board[row][i] != null && this.board[row][i].getPlayer().equals(turnPlayer)) {
                 count++;
                 if (count == 4) {
@@ -65,7 +65,7 @@ public class Game {
 
     private boolean checkVertical(int col, Player turnPlayer) {
         int count = 0;
-        for (int i = 0; i < this.numRows; i++) {
+        for (int i = 0; i < this.NUM_ROWS; i++) {
             if (this.board[i][col] != null && this.board[i][col].getPlayer().equals(turnPlayer)) {
                 count++;
                 if (count == 4) {
@@ -87,7 +87,7 @@ public class Game {
             i--;
             j--;
         }
-        while (i < numRows && j < numCols) {
+        while (i < NUM_ROWS && j < NUM_COLS) {
             if (this.board[i][j] != null && this.board[i][j].getPlayer().equals(turnPlayer)) {
                 count++;
                 if (count == 4) {
@@ -104,11 +104,11 @@ public class Game {
         count = 0;
         i = row;
         j = col;
-        while (i < numRows - 1 && j > 0) {
+        while (i < NUM_ROWS - 1 && j > 0) {
             i++;
             j--;
         }
-        while (i >= 0 && j < numCols) {
+        while (i >= 0 && j < NUM_COLS) {
             if (this.board[i][j] != null && this.board[i][j].getPlayer().equals(turnPlayer)) {
                 count++;
                 if (count == 4) {
@@ -134,7 +134,7 @@ public class Game {
         // Whose disc to insert?
         Disc disc = getTurnPlayer().getType() == PlayerType.PLAYER_ONE ? new PlayerOneDisc(getTurnPlayer()) : new PlayerTwoDisc(getTurnPlayer());
         // Check if the column is valid
-        if (col < 0 || col >= numCols) {
+        if (col < 0 || col >= NUM_COLS) {
             throw new InvalidColumnException();
         }
         // Check if the column is full
@@ -142,13 +142,13 @@ public class Game {
             throw new ColumnIsFullException();
         }
         // Insert the disc
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < NUM_ROWS; i++) {
             if (board[i][col] != null) {
                 board[i - 1][col] = disc;
                 return checkWin(i - 1, col, getTurnPlayer());
             }
         }
-        board[numRows - 1][col] = disc;
-        return checkWin(numRows - 1, col, getTurnPlayer());
+        board[NUM_ROWS - 1][col] = disc;
+        return checkWin(NUM_ROWS - 1, col, getTurnPlayer());
     }
 }
--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,16 +1,20 @@
 package view;
 
+import model.Disc;
 import model.Game;
 import model.Player;
 import model.PlayerType;
 
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class GameView {
     Scanner scanner;
+
     public GameView() {
         scanner = new Scanner(System.in);
     }
+
     public Game makeGame() {
         System.out.println("Welcome to Connect Four!");
         System.out.println("Enter the first player's name: ");
@@ -21,5 +25,32 @@ public class GameView {
                 playerTwo = new Player(PlayerType.PLAYER_TWO, playerTwoName.toUpperCase());
         Game game = new Game(playerOne, playerTwo);
         return game;
+    }
+
+    public void printBoard(Game game) {
+        System.out.println("*** Connect Four ***");
+        for (Disc[] row : game.getBoard()) {
+            StringJoiner sj = new StringJoiner(" | ");
+            for (Disc disc : row) {
+                if (disc == null) {
+                    sj.add(" ");
+                } else {
+                    sj.add(disc.toString());
+                }
+            }
+            System.out.println(sj.toString());
+        }
+    }
+
+    public int playTurn(String playerName) {
+        System.out.println(playerName + ", enter a column number: ");
+        int columnToInsertIn;
+        String input = scanner.nextLine();
+        try {
+            columnToInsertIn = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Invalid input. Please enter a number.");
+        }
+        return columnToInsertIn;
     }
 }
